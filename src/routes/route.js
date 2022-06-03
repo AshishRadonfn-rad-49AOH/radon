c103d7d5a8b9be16e437e32869e7d53bf694d517
AshishRadonfn-rad-49AOH <ashishranjan510360@gmail.com>
2022-06-03 13:30:01 +0530

    learning the post api

--- src/routes/route.js
+++ src/routes/route.js
@@ -157,15 +157,49 @@ router.get('/films/:filmId', function(req, res){
         res.send(  { data: missingNumber  }  );
       });
          
-      
     
+      let players =
+      [
+          {
+              "name": "manish",
+              "dob": "1/1/1995",
+              "gender": "male",
+              "city": "jalandhar",
+              "sports": [
+                  "swimming"
+              ]
+          },
+          {
+              "name": "gopal",
+              "dob": "1/09/1995",
+              "gender": "male",
+              "city": "delhi",
+              "sports": [
+                  "soccer"
+              ]
+          },
+          {
+              "name": "lokesh",
+              "dob": "1/1/1990",
+              "gender": "male",
+              "city": "mumbai",
+              "sports": [
+                  "soccer"
+              ]
+          },
+      ]
+
       
-
-
+      router.post('/players', function (req, res) {
+ 
+        let ele = req.body.element
+        res.send(  { data: players , status: true }  )
+    })
+ 
 
 
 
 
 
 module.exports = router;
-// adding this comment for no reason    git
+// adding this comment for no reason    
