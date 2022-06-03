85851e50bf58d6b1a5a40342892c4dd36a809447
AshishRadonfn-rad-49AOH <ashishranjan510360@gmail.com>
2022-06-03 09:44:18 +0530

    git API assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,32 @@
 const express = require('express');
-const myHelper = require('../util/helper')
 const underscore = require('underscore')
+const lodash = require('lodash')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
-});
 
 router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
+    let month = ["January","Feburary","March","April","May","June","July","August","September","October","November","December"]
+    let getMonth = lodash.chunk(month,4)
+
+    let arr = [1,3,5,7,9,11,13,15,17,19]
+    let getoddnum = lodash.tail(arr,9)
+
+    let getunion= lodash.union([1,2,3,4,5],
+                               [2,3,4,5,6],
+                               [3,4,5,6,7],
+                               [4,5,6,7,8],
+                               [5,6,7,8,9])
+
+    let movies = [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+    let getmovies = lodash.fromPairs(movies)
+    
+    res.send('watching movies!')
+    console.log(getMonth)
+    console.log(getoddnum)
+    console.log(getunion)
+    console.log(getmovies)
+
 });
 
 router.get('/candidates', function(req, res){
@@ -36,6 +47,124 @@ router.get('/candidates/:canidatesName', function(req, res){
     res.send('Done')
 })
 
+router.get('/movies', function(req, res){
+    const movies = ["Rang de basanti","The shining","Lord of the rings","Batman begins"]
+   console.log(JSON.stringify(movies))
+
+   res.send('Hello there thats it!')
+})
+
+router.get('/movies/:indexNumber', function(req, res){
+    let movies = ["Rang de basanti","The shining","Lord of the rings","Batman begins"]   
+    if(req.params.indexNumber<movies.length){
+        console.log(movies[req.params.indexNumber])
+     } else{
+        console.log('invalid index')
+    }
+    res.send('Hello there do it!')
+})
+
+//router.get('/movies/:indexNumber', function(req, res){
+     //let movies = ["Rang de basanti","The shining","Lord of the rings","Batman begins"]
+    //console.log(req)
+    
+    //res.send(movies[req.params.indexNumber])
+    //let movies = ["Rang de basanti","The shining","Lord of the rings","Batman begins"]
+    //console.log(req)
+   // if(req.params.indexNumber >= movies.length)
+    //{
+      //  res.send('not found') 
+    //} else{
+       // res.send(movies[req.params.indexNumber])
+    //}
+//})
+
+router.get('/films', function(req, res){
+    let films = [ {
+        "id": 1,
+        "name" : "The Shining"
+       }, {
+        id: 2,
+        "name": "ncendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       
+   console.log(JSON.stringify(films))
+
+   res.send(films)
+})
+
+router.get('/films/:filmId', function(req, res){ 
+    let films = [ {
+        "id": 1,
+        "name" : "The Shining"
+       }, {
+        id: 2,
+        "name": "ncendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       if ( 0 >= req.params.filmId ||  films.length < req.params.filmId) {
+        res.send("No movie exists with this id");
+        console.log("No movie exists with this id")
+      } else {
+        const filmData = films.find(ele => ele.id === +req.params.filmId)
+        res.send(filmData);
+        console.log(filmData);
+      }
+        })
+
+
+    router.get("/sol1", function (req, res) {
+        //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+        let arr= [1,2,3,5,6,7]
+      
+        let total = 0;
+        for (var i in arr) {
+            total += arr[i];
+        }
+      
+        let lastDigit= arr.pop()
+        let consecutiveSum= lastDigit * (lastDigit+1) / 2
+        let missingNumber= consecutiveSum - total
+      
+        res.send(  { data: missingNumber  }  );
+      });
+    router.get("/sol2", function (req, res) {
+        //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+        let arr= [33, 34, 35, 37, 38]
+        let len= arr.length
+      
+        let total = 0;
+        for (var i in arr) {
+            total += arr[i];
+        }
+      
+        let firstDigit= arr[0]
+        let lastDigit= arr.pop()
+        let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+        let missingNumber= consecutiveSum - total
+       
+        res.send(  { data: missingNumber  }  );
+      });
+         
+    
+      
+
+
+
+
+
+
 
 module.exports = router;
-// adding this comment for no reason
+// adding this comment for no reason    
